eae6f444e785323a21a2e5ffdf10c0ad85168663
Tzvetomir Stoyanov <tz.stoyanov@gmail.com>
2025-05-04 19:01:29 +0300

    Introduce minimal generic application.
    Added minimal application - just a system main loop. It uses the system
    modules, depending of the configuration from params.txt file.
    
    Signed-off-by: Tzvetomir Stoyanov <tz.stoyanov@gmail.com>

---
+++ app/common/main.c
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Copyright (C) 2025, Tzvetomir Stoyanov <tz.stoyanov@gmail.com>
+ */
+
+#include "common_lib.h"
+
+int main(void)
+{
+	system_common_main();
+}
