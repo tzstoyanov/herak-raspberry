8d83e4880cddaed8dab25958e0877cf2480049ed
Tzvetomir Stoyanov <tz.stoyanov@gmail.com>
2025-05-10 06:26:24 +0300

    Increase the locking scope of IP stack lock macros
    Use SYS_ARCH_PROTECT to protect all IP stack related APIs.
    
    Signed-off-by: Tzvetomir Stoyanov <tz.stoyanov@gmail.com>

--- libs/common/src/common_internal.h
+++ libs/common/src/common_internal.h
@@ -18,9 +18,9 @@ extern "C" {
 
 #define HTTP_USER_AGENT		"PicoW"
 
-#define LWIP_LOCK_START	cyw43_arch_lwip_begin();
-#define LWIP_LOCK_END	cyw43_arch_lwip_end();
-/*
+//#define LWIP_LOCK_START	cyw43_arch_lwip_begin();
+//#define LWIP_LOCK_END	cyw43_arch_lwip_end();
+
 #define LWIP_LOCK_START { \
 		SYS_ARCH_DECL_PROTECT(__lev__); \
 		SYS_ARCH_PROTECT(__lev__); \
@@ -29,7 +29,7 @@ extern "C" {
 #define LWIP_LOCK_END \
 	cyw43_arch_lwip_end(); \
 	SYS_ARCH_UNPROTECT(__lev__); }
-*/
+
 
 #define SYS_LOCK_START { \
 		SYS_ARCH_DECL_PROTECT(__lev__); \
