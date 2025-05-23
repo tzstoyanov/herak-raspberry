fac3a78b50cf3ebac333ee051ea8b712785f2d18
Tzvetomir Stoyanov <tz.stoyanov@gmail.com>
2025-05-23 08:09:49 +0300

    Make files refactoring.
    Simplified the application CMakefile. Keep only application specific
    rules in these files, moved all other rules in the common library
    CMakefile.
    Introduced compile time switches in application CMakefile, used to
    select which modules are part of the application.
    
    Signed-off-by: Tzvetomir Stoyanov <tz.stoyanov@gmail.com>

---
+++ include/herak_sys.h
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Copyright (C) 2023, Tzvetomir Stoyanov <tz.stoyanov@gmail.com>
+ */
+
+#ifndef _LIB_HERAK_SYS_H_
+#define _LIB_HERAK_SYS_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void system_common_main(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _LIB_HERAK_SYS_H_ */
