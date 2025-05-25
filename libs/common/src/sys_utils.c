4796245c97e49f67d715b6a92e53183b34522f17
Tzvetomir Stoyanov <tz.stoyanov@gmail.com>
2025-05-25 08:29:42 +0300

    Helper functions refactoring.
    Moved helper functions in sys_utils.c.
    
    Signed-off-by: Tzvetomir Stoyanov <tz.stoyanov@gmail.com>

--- libs/common/src/sys_utils.c
+++ libs/common/src/sys_utils.c
@@ -5,10 +5,14 @@
 
 #include <stdio.h>
 #include <stdarg.h>
+#include <malloc.h>
 
+#include "pico/cyw43_arch.h"
 #include "common_lib.h"
 #include "common_internal.h"
 #include "pico/stdlib.h"
+
+extern char __StackLimit, __bss_end__;
 
 int sys_asprintf(char **strp, const char *fmt, ...)
 {
@@ -30,6 +34,36 @@ int sys_asprintf(char **strp, const char *fmt, ...)
 	va_end(args);
 	return size;
 }
+#define PRINT_BUF_LEN	32
+static void dump_raw_data(char *topic, char *format, const uint8_t *data, int len)
+{
+	char print_buff[PRINT_BUF_LEN], buf[4];
+	int i = 0, j = 0;
+
+	print_buff[0] = 0;
+	while (i < len) {
+		snprintf(buf, 4, format, data[i++]);
+		if ((j + strlen(buf)) >= PRINT_BUF_LEN) {
+			j = 0;
+			hlog_info(topic, "\t %s", print_buff);
+			print_buff[0] = 0;
+		}
+		strcat(print_buff, buf);
+		j += strlen(buf);
+	}
+	if (j)
+		hlog_info(topic, "\t %s", print_buff);
+}
+
+void dump_hex_data(char *topic, const uint8_t *data, int len)
+{
+	dump_raw_data(topic, "%0.2X ", data, len);
+}
+
+void dump_char_data(char *topic, const uint8_t *data, int len)
+{
+	dump_raw_data(topic, "%c", data, len);
+}
 
 uint8_t sys_value_to_percent(uint32_t range_min, uint32_t range_max, uint32_t val)
 {
@@ -39,4 +73,48 @@ uint8_t sys_value_to_percent(uint32_t range_min, uint32_t range_max, uint32_t va
 		return 100;
 
 	return	(100 * (val - range_min)) / (range_max - range_min);
+}
+
+uint32_t samples_filter(uint32_t *samples, int total_count, int filter_count)
+{
+	uint32_t all;
+	uint16_t sw;
+	int i, j;
+
+	/* bubble sort */
+	for (i = 0 ; i < total_count - 1; i++) {
+		for (j = 0 ; j < total_count - i - 1; j++) {
+			if (samples[j] > samples[j+1]) {
+				sw  = samples[j];
+				samples[j] = samples[j+1];
+				samples[j+1] = sw;
+			}
+		}
+	}
+	/* filter biggest and smallest */
+	all = 0;
+	for (i = filter_count ; i < total_count - filter_count; i++)
+		all += samples[i];
+	all /= total_count-(2*filter_count);
+
+	return all;
+}
+
+uint32_t get_total_heap(void)
+{
+	static uint32_t mem_total;
+
+	if (!mem_total)
+		mem_total = &__StackLimit  - &__bss_end__;
+
+	return mem_total;
+}
+
+uint32_t get_free_heap(void)
+{
+	struct mallinfo m = mallinfo();
+
+//	return m.fordblks;
+	return get_total_heap() - m.uordblks;
+
 }
