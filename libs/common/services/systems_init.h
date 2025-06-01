2c1750b65aedf44e4057852681660da7e61c574d
Tzvetomir Stoyanov <tz.stoyanov@gmail.com>
2025-06-01 08:51:42 +0300

    Add infrastructure for service modules
    Services are implemented as part of herak common library. Currently the
    library builds all available services, although not all of them are used
    by all applications. Added initial infrastructure to move services
    implementation as system modules. System modules are more flexible, as they
    can be easily added / removed from the build.
    
    Signed-off-by: Tzvetomir Stoyanov <tz.stoyanov@gmail.com>

---
+++ libs/common/services/systems_init.h
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Copyright (C) 2025, Tzvetomir Stoyanov <tz.stoyanov@gmail.com>
+ */
+
+#ifndef _SYSTEMS_INIT_H_
+#define _SYSTEMS_INIT_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void systems_register_and_init(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _SYSTEMS_INIT_H_ */
