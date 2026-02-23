93e4907e21cacac0d2f5663824717de64c296c06
Tzvetomir Stoyanov <tz.stoyanov@gmail.com>
2026-02-23 06:10:09 +0200

    SHT20: Add support for multiple sensors on same I2C buss
    The SHT20 sensors have same I2C address, which causes collisions when
    multiple of them are attached to same I2C buss. To avoid collisions in
    such cases, refactored the SHT20 read logic:
    - All sensors are powered down by default.
    - Power up only the sensor which is being read. When the read completes,
      power down the sensor.
    
    Signed-off-by: Tzvetomir Stoyanov <tz.stoyanov@gmail.com>

--- libs/common/devices/sht20/sht20.c
+++ libs/common/devices/sht20/sht20.c
@@ -16,23 +16,28 @@
 #include "base64.h"
 #include "params.h"
 
-#define SHT20_MODULE    "sht20"
-#define MEASURE_TIME_MS 100
-#define SHT20_SENORS_MAX    6
-#define READ_INTERVAL_MS 1000
-#define MQTT_DATA_LEN   128
-#define MQTT_DELAY_MS 5000
+#define SHT20_MODULE		"sht20"
+#define SHT20_SENORS_MAX	6
+#define MQTT_DATA_LEN	128
+#define MQTT_DELAY_MS	5000
 
-#define I2C_TIMEOUT_US  1000
-#define SHT20_ADDR 0x40
-#define SHT20_CLOCK 100000
-#define SHT20_DATA_SIZE 3
-#define SHT20_CMD_RETRY 3
-#define SHT0_POWER_DELAY_MS	20
+#define I2C_TIMEOUT_US	1000
+#define SHT20_ADDR	0x40
+#define SHT20_CLOCK	50000
+#define SHT20_DATA_SIZE	3
+#define SHT20_CMD_RETRY	5
 
-				// no hold	// hold
-#define SHT20_TEMP		0xF3		// 0xE3
-#define SHT20_HUMID		0xF5		// 0xE5
+#define SHT0_READ_INTERVAL_MS		1000
+#define SHT0_MEASURE_DELAY_MS		100
+#define SHT0_POWER_UP_DELAY_MS		50
+#define SHT0_POWER_DOWN_DELAY_MS	100
+// 1 min delay in power down state if more than 20 connection errors are detected
+#define CONN_ERR_THR			20
+#define SHT0_ERROR_DELAY_MS		60000
+
+					// no hold	// hold
+#define SHT20_TEMP		0xF3	// 0xE3
+#define SHT20_HUMID		0xF5	// 0xE5
 #define SHT20_WRITE_USER_REG	0xE6
 #define SHT20_READ_USER_REG	0xE7
 #define SHT20_RESET		0xFE
@@ -45,7 +50,7 @@
 #define SHT20_CFG_DISABLE_ONCHIP_HEATER	0x00
 #define SHT20_CFG_DISABLE_OTP_RELOAD    0x02
 
-#define CONN_ERR_THR	5
+#define IS_DEBUG(C)	((C)->debug)
 
 static const int16_t __in_flash() POLYNOMIAL = 0x131;
 
@@ -56,6 +61,8 @@ enum {
 	SHT20_MQTT_DEW_POINT,
 	SHT20_MQTT_MAX,
 };
+
+struct sht20_context_t;
 
 struct sht20_sensor {
 	i2c_inst_t *i2c;
@@ -70,19 +77,22 @@ struct sht20_sensor {
 	bool force;
 	bool connected;
 	bool hard_reset;
-	uint16_t conn_err_count;
-	uint64_t hard_reset_count;
-	uint64_t soft_reset_count;
+	uint64_t conn_err_count;
+	uint64_t err_state;
 	uint8_t config;
 	uint8_t read_cmd;
 	uint64_t read_requested;
-	uint64_t last_read;
 	mqtt_component_t mqtt_comp[SHT20_MQTT_MAX];
+	struct sht20_context_t *ctx;
+	uint64_t ok_stat;
+	uint64_t err_stat;
 };
 
 struct sht20_context_t {
 	sys_module_t mod;
 	uint8_t count;
+	uint8_t idx;
+	uint64_t last_read;
 	struct sht20_sensor *sensors[SHT20_SENORS_MAX];
 	uint32_t debug;
 	uint64_t mqtt_last_send;
@@ -107,30 +117,65 @@ static int sht20_sensor_read(struct sht20_sensor *sensor, uint8_t *cmd, uint8_t 
 	return -1;
 }
 
+// https://github.com/u-fire/uFire_SHT20/blob/master/src/uFire_SHT20.cpp
+// https://github.com/keep1234quiet/IOT_SHT20_STM32_ESP8266/blob/master/Devices/sht20.c
+static void sht20_power_down(struct sht20_sensor *sensor)
+{
+	i2c_deinit(sensor->i2c);
+
+	// Set pins to High-Impedance (Input) without pulling them low first
+	gpio_set_function(sensor->sda_pin, GPIO_FUNC_NULL);
+	gpio_set_function(sensor->scl_pin, GPIO_FUNC_NULL);
+	gpio_set_dir(sensor->sda_pin, GPIO_IN);
+	gpio_set_dir(sensor->scl_pin, GPIO_IN);
+	gpio_disable_pulls(sensor->sda_pin);
+	gpio_disable_pulls(sensor->scl_pin);
+
+	if (sensor->power_pin >= GPIO_PIN_MIN)
+		gpio_put(sensor->power_pin, 0);
+
+	sleep_ms(SHT0_POWER_DOWN_DELAY_MS);
+}
+
+static void sht20_power_up(struct sht20_sensor *sensor)
+{
+	// Power on the sensor
+	if (sensor->power_pin >= GPIO_PIN_MIN) {
+		gpio_init(sensor->power_pin);
+		gpio_set_dir(sensor->power_pin, GPIO_OUT);
+		gpio_put(sensor->power_pin, 1);
+	}
+	sleep_ms(SHT0_POWER_UP_DELAY_MS);
+
+	// BUS recovery: Force SCL high/low to clear any stuck bits
+	gpio_init(sensor->sda_pin);
+	gpio_init(sensor->scl_pin);
+	gpio_set_dir(sensor->sda_pin, GPIO_IN);
+	gpio_set_dir(sensor->scl_pin, GPIO_OUT);
+	gpio_pull_up(sensor->sda_pin);
+	gpio_pull_up(sensor->scl_pin);
+	sleep_ms(5);
+	for (int i = 0; i < 9; i++) {
+		gpio_put(sensor->scl_pin, 1);
+		sleep_us(5);
+		gpio_put(sensor->scl_pin, 0);
+		sleep_us(5);
+	}
+	gpio_put(sensor->scl_pin, 1);
+
+	// Init the I2C hardware
+	i2c_init(sensor->i2c, SHT20_CLOCK);
+	gpio_set_function(sensor->sda_pin, GPIO_FUNC_I2C);
+	gpio_set_function(sensor->scl_pin, GPIO_FUNC_I2C);
+}
+
 static int sht20_sensor_init(struct sht20_sensor *sensor)
 {
 	int ret = -1;
 
 	sensor->connected = false;
-	if (sensor->conn_err_count >= CONN_ERR_THR) {
-		sensor->hard_reset = true;
-		sensor->conn_err_count = 0;
-	}
+	sht20_power_up(sensor);
 
-	if (sensor->power_pin >= GPIO_PIN_MIN && sensor->hard_reset) {
-		gpio_put(sensor->power_pin, 0);
-		sleep_ms(SHT0_POWER_DELAY_MS);
-		gpio_put(sensor->power_pin, 1);
-		sleep_ms(SHT0_POWER_DELAY_MS);
-		sensor->hard_reset = false;
-		sensor->hard_reset_count++;
-	} else {
-		sensor->soft_reset_count++;
-	}
-
-	if (sht20_sensor_write(sensor, SHT20_RESET))
-		goto out;
-	sleep_ms(SHT0_POWER_DELAY_MS);
 	if (sht20_sensor_write(sensor, SHT20_READ_USER_REG))
 		goto out;
 	if (sht20_sensor_read(sensor, &sensor->config, sizeof(sensor->config)))
@@ -146,13 +191,18 @@ static int sht20_sensor_init(struct sht20_sensor *sensor)
 	if (sht20_sensor_write(sensor, sensor->config))
 		goto out;
 
-	sensor->read_cmd = SHT20_TEMP;
 	sensor->connected = true;
+	sensor->conn_err_count = 0;
 	ret = 0;
 out:
-	if (!sensor->connected)
+	if (!sensor->connected) {
+		sensor->err_stat++;
 		sensor->conn_err_count++;
-	sensor->last_read = time_ms_since_boot();
+		if (!sensor->err_state && !(sensor->conn_err_count % CONN_ERR_THR))
+			sensor->err_state = time_ms_since_boot();
+		if (IS_DEBUG(sensor->ctx))
+			hlog_info(SHT20_MODULE, "Connection error on sensor %d: %d", sensor->sda_pin, sensor->conn_err_count);
+	}
 	return ret;
 }
 
@@ -166,27 +216,6 @@ static void sht20_sensor_check_connected(struct sht20_sensor *sensor)
 	if (sensor->config == 0xFF)
 		return;
 	sensor->connected = true;
-}
-
-// https://github.com/u-fire/uFire_SHT20/blob/master/src/uFire_SHT20.cpp
-// https://github.com/keep1234quiet/IOT_SHT20_STM32_ESP8266/blob/master/Devices/sht20.c
-static void sht20_i2c_init(struct sht20_context_t *ctx)
-{
-	int i;
-
-	for (i = 0; i < ctx->count; i++) {
-		if (ctx->sensors[i]->power_pin >= GPIO_PIN_MIN) {
-			gpio_init(ctx->sensors[i]->power_pin);
-			gpio_set_dir(ctx->sensors[i]->power_pin, GPIO_OUT);
-			gpio_put(ctx->sensors[i]->power_pin, 0);
-			ctx->sensors[i]->hard_reset = true;
-		}
-		i2c_init(ctx->sensors[i]->i2c, SHT20_CLOCK);
-		gpio_set_function(ctx->sensors[i]->sda_pin, GPIO_FUNC_I2C);
-		gpio_set_function(ctx->sensors[i]->scl_pin, GPIO_FUNC_I2C);
-		gpio_pull_up(ctx->sensors[i]->sda_pin);
-		gpio_pull_up(ctx->sensors[i]->scl_pin);
-	}
 }
 
 static bool init_sht20_i2c_params(struct sht20_sensor *sensor)
@@ -246,6 +275,7 @@ static bool sht20_config_get(struct sht20_context_t **ctx)
 		(*ctx)->sensors[(*ctx)->count] = calloc(1, sizeof(struct sht20_sensor));
 		if (!(*ctx)->sensors[(*ctx)->count])
 			continue;
+		sensor.ctx = *ctx;
 		memcpy((*ctx)->sensors[(*ctx)->count], &sensor, sizeof(sensor));
 		(*ctx)->count++;
 		if ((*ctx)->count >= SHT20_SENORS_MAX)
@@ -276,16 +306,17 @@ out:
 static bool sht20_log(void *context)
 {
 	struct sht20_context_t *ctx = (struct sht20_context_t *)context;
+	uint32_t q;
 	int i;
 
 	if (!ctx)
 		return true;
 	hlog_info(SHT20_MODULE, "Reading %d sensors:", ctx->count);
 	for (i = 0; i < ctx->count; i++) {
-		hlog_info(SHT20_MODULE, "\tid %d attached to %d,%d(%s), resets (s: %lld; h: %lld), power pin (%d)",
+		q = ((ctx->sensors[i]->ok_stat * 100) / (ctx->sensors[i]->ok_stat + ctx->sensors[i]->err_stat));
+		hlog_info(SHT20_MODULE, "\tid %d attached to %d,%d(%s %d%%), power pin (%d)",
 				  i, ctx->sensors[i]->sda_pin, ctx->sensors[i]->scl_pin,
-				  ctx->sensors[i]->connected ? "connected" : "not connected",
-				  ctx->sensors[i]->soft_reset_count, ctx->sensors[i]->hard_reset_count,
+				  ctx->sensors[i]->connected ? "connected" : "not connected", q,
 				  ctx->sensors[i]->power_pin);
 		hlog_info(SHT20_MODULE, "\t\tTemperature %3.2f°C, Humidity %3.2f%%, VPD %3.2fkPa, Dew Point %3.2f%%",
 				  ctx->sensors[i]->temperature, ctx->sensors[i]->humidity,
@@ -339,11 +370,14 @@ static int sth20_mqtt_data_send(struct sht20_context_t *ctx, int idx)
 static void sht20_mqtt_send(struct sht20_context_t *ctx)
 {
 	uint64_t now = time_ms_since_boot();
-	static int idx;
+	bool refresh;
 	int i;
 
+	if ((now - ctx->mqtt_last_send) >= MQTT_DELAY_MS)
+		refresh = true;
+
 	for (i = 0; i < ctx->count; i++)
-		if (ctx->sensors[i]->force)
+		if (refresh || ctx->sensors[i]->force)
 			ctx->sensors[i]->mqtt_comp[0].force = true;
 
 	for (i = 0; i < ctx->count; i++) {
@@ -352,30 +386,28 @@ static void sht20_mqtt_send(struct sht20_context_t *ctx)
 			return;
 		}
 	}
-
-	if ((now - ctx->mqtt_last_send) < MQTT_DELAY_MS)
-		return;
-	if (idx >= ctx->count)
-		idx = 0;
-	sth20_mqtt_data_send(ctx, idx++);
 }
 
-static int sht20_sensor_request_data(struct sht20_context_t *ctx, int idx)
+static int sht20_sensor_request_data(struct sht20_sensor *sensor)
 {
 	int ret;
 
-	ret = i2c_write_blocking(ctx->sensors[idx]->i2c,
-							 ctx->sensors[idx]->sht20_addr,
-							 &ctx->sensors[idx]->read_cmd,
-							 sizeof(ctx->sensors[idx]->read_cmd), false);
-	if (ret != sizeof(ctx->sensors[idx]->read_cmd))
+	ret = i2c_write_blocking(sensor->i2c,
+							 sensor->sht20_addr,
+							 &(sensor->read_cmd),
+							 sizeof(sensor->read_cmd), false);
+	if (ret != sizeof(sensor->read_cmd)) {
+		sensor->err_stat++;
+		if (IS_DEBUG(sensor->ctx))
+			hlog_info(SHT20_MODULE, "Failed to request data from sensor %d", sensor->sda_pin);
 		return -1;
+	}
 
-	ctx->sensors[idx]->read_requested = time_ms_since_boot();
+	sensor->read_requested = time_ms_since_boot();
 	return 0;
 }
 
-static int sht20_check_crc(struct sht20_context_t *ctx, uint8_t *data, uint8_t count, uint8_t checksum)
+static int sht20_check_crc(struct sht20_sensor *sensor, uint8_t *data, uint8_t count, uint8_t checksum)
 {
 	uint8_t crc = 0;
 	int i, j;
@@ -391,109 +423,131 @@ static int sht20_check_crc(struct sht20_context_t *ctx, uint8_t *data, uint8_t c
 		}
 	}
 	if (crc != checksum) {
-		if (ctx->debug)
-			hlog_info(SHT20_MODULE, "CRC error: %d != %d", crc, checksum);
+		if (IS_DEBUG(sensor->ctx))
+			hlog_info(SHT20_MODULE, "CRC error on sensor %d: %d != %d", sensor->sda_pin, crc, checksum);
 		return -1;
 	}
 
 	return 0;
 }
 
-static int sht20_sensor_get_data(struct sht20_context_t *ctx, int idx)
+static int sht20_sensor_get_data(struct sht20_sensor *sensor)
 {
 	uint8_t buff[SHT20_DATA_SIZE];
 	float data, f1, f2;
 	uint16_t raw;
 	int ret = -1;
 
-	ret = i2c_read_blocking(ctx->sensors[idx]->i2c,
-							ctx->sensors[idx]->sht20_addr, buff, SHT20_DATA_SIZE, false);
+	ret = i2c_read_blocking(sensor->i2c, sensor->sht20_addr, buff, SHT20_DATA_SIZE, false);
 	if (ret != SHT20_DATA_SIZE)
 		goto out;
 
-	if (ctx->debug)
-		hlog_info(SHT20_MODULE, "Got raw data: [0x%2X 0x%2X 0x%2X]", buff[0], buff[1], buff[2]);
-	ret = sht20_check_crc(ctx, buff, 2, buff[2]);
+	if (IS_DEBUG(sensor->ctx))
+		hlog_info(SHT20_MODULE, "Got raw data from sensor %d: [0x%2X 0x%2X 0x%2X]",
+				  sensor->sda_pin, buff[0], buff[1], buff[2]);
+	ret = sht20_check_crc(sensor, buff, 2, buff[2]);
 	if (ret)
 		goto out;
 
 	raw = (buff[0] << 8) + buff[1];
 
-	if (ctx->sensors[idx]->read_cmd == SHT20_TEMP) {
+	if (sensor->read_cmd == SHT20_TEMP) {
 		data = raw * (175.72 / 65536.0)-46.85;
-		if (ctx->sensors[idx]->temperature != data) {
-			ctx->sensors[idx]->force = true;
-			ctx->sensors[idx]->temperature = data;
+		if (sensor->temperature != data) {
+			sensor->force = true;
+			sensor->temperature = data;
 		}
-		ctx->sensors[idx]->read_cmd = SHT20_HUMID;
+		sensor->read_cmd = SHT20_HUMID;
 	} else {
 		data = raw * (125.0 / 65536.0) - 6.0;
-		if (ctx->sensors[idx]->humidity != data) {
-			ctx->sensors[idx]->force = true;
-			ctx->sensors[idx]->humidity = data;
+		if (sensor->humidity != data) {
+			sensor->force = true;
+			sensor->humidity = data;
 		}
-		ctx->sensors[idx]->read_cmd = SHT20_TEMP;
+		sensor->read_cmd = SHT20_TEMP;
 	}
 
-	f1 = 0.6108 * exp(17.27 * ctx->sensors[idx]->temperature / (ctx->sensors[idx]->temperature + 237.3));
-	f2 = ctx->sensors[idx]->humidity / 100 * f1;
+	f1 = 0.6108 * exp(17.27 * sensor->temperature / (sensor->temperature + 237.3));
+	f2 = sensor->humidity / 100 * f1;
 	data = f1 - f2;
-	if (ctx->sensors[idx]->vpd != data) {
-		ctx->sensors[idx]->force = true;
-		ctx->sensors[idx]->vpd = data;
+	if (sensor->vpd != data) {
+		sensor->force = true;
+		sensor->vpd = data;
 	}
 
-	f1 = -1.0 * ctx->sensors[idx]->temperature;
+	f1 = -1.0 * sensor->temperature;
 	f2 = 6.112 * exp(-1.0 * 17.67 * f1 / (243.5 - f1));
-	f1 = ctx->sensors[idx]->humidity / 100.0 * f2;
+	f1 = sensor->humidity / 100.0 * f2;
 	f2 = log(f1 / 6.112);
 	data = -243.5 * f2 / (f2 - 17.67);
-	if (ctx->sensors[idx]->dew_point != data) {
-		ctx->sensors[idx]->force = true;
-		ctx->sensors[idx]->dew_point = data;
+	if (sensor->dew_point != data) {
+		sensor->force = true;
+		sensor->dew_point = data;
 	}
 
-	if (ctx->debug)
-		hlog_info(SHT20_MODULE, "temperature %3.2f,  humidity %3.2f, vpd %3.2f, dew_point %3.2f",
-				  ctx->sensors[idx]->temperature, ctx->sensors[idx]->humidity,
-				  ctx->sensors[idx]->vpd, ctx->sensors[idx]->dew_point);
+	if (IS_DEBUG(sensor->ctx))
+		hlog_info(SHT20_MODULE, "   temperature %3.2f,  humidity %3.2f, vpd %3.2f, dew_point %3.2f",
+				  sensor->temperature, sensor->humidity, sensor->vpd, sensor->dew_point);
 
-	ctx->sensors[idx]->last_read = time_ms_since_boot();
 	ret = 0;
 
 out:
-	ctx->sensors[idx]->read_requested = 0;
+	sensor->read_requested = 0;
+	if (ret)
+		sensor->err_stat++;
+	else
+		sensor->ok_stat++;
 	return ret;
 }
 
-static void sht20_sensor_data(struct sht20_context_t *ctx, int idx)
+static int sht20_sensor_data(struct sht20_sensor *sensor)
 {
 	uint64_t now = time_ms_since_boot();
+	int ret = -1;
 
-	if (ctx->sensors[idx]->read_requested) {
-		if ((now - ctx->sensors[idx]->read_requested) < MEASURE_TIME_MS)
-			return;
-		sht20_sensor_get_data(ctx, idx);
-		return;
+	if (sensor->err_state) {
+		if ((now - sensor->err_state) > SHT0_ERROR_DELAY_MS)
+			sensor->err_state = 0;
+		return -1;
 	}
 
-	if (((now - ctx->sensors[idx]->last_read) < READ_INTERVAL_MS))
-		return;
-	sht20_sensor_check_connected(ctx->sensors[idx]);
-	if (ctx->sensors[idx]->connected)
-		sht20_sensor_request_data(ctx, idx);
-	else
-		sht20_sensor_init(ctx->sensors[idx]);
+	if (sensor->connected && sensor->read_requested) {
+		if ((now - sensor->read_requested) < SHT0_MEASURE_DELAY_MS)
+			return 0;
+		sht20_sensor_get_data(sensor);
+		sht20_power_down(sensor);
+		return 1;
+	}
+
+	sht20_sensor_init(sensor);
+	sht20_sensor_check_connected(sensor);
+	if (sensor->connected)
+		ret = sht20_sensor_request_data(sensor);
+
+	if (ret)
+		sht20_power_down(sensor);
+
+	return ret;
 }
 
 static void sht20_run(void *context)
 {
 	struct sht20_context_t *ctx = (struct sht20_context_t *)context;
-	int i;
+	uint64_t now = time_ms_since_boot();
 
-	for (i = 0; i < ctx->count; i++)
-		sht20_sensor_data(ctx, i);
+	if (ctx->idx < ctx->count) {
+		if (sht20_sensor_data(ctx->sensors[ctx->idx])) {
+			ctx->last_read = now;
+			ctx->idx++;
+		}
+		sht20_mqtt_send(ctx);
+		return;
+	}
+
 	sht20_mqtt_send(ctx);
+	if (((now - ctx->last_read) < SHT0_READ_INTERVAL_MS))
+		return;
+	ctx->idx = 0;
 }
 
 static void sht20_mqtt_components_add(struct sht20_context_t *ctx)
@@ -544,9 +598,10 @@ static bool sht20_init(struct sht20_context_t **ctx)
 
 	if (!sht20_config_get(ctx))
 		return false;
-	sht20_i2c_init(*ctx);
-	for (i = 0; i < (*ctx)->count; i++)
-		sht20_sensor_init((*ctx)->sensors[i]);
+	for (i = 0; i < (*ctx)->count; i++) {
+		sht20_power_down((*ctx)->sensors[i]);
+		(*ctx)->sensors[i]->read_cmd = SHT20_TEMP;
+	}
 	sht20_mqtt_components_add(*ctx);
 	hlog_info(SHT20_MODULE, "Initialise successfully %d sensors", (*ctx)->count);
 	for (i = 0; i < (*ctx)->count; i++)
